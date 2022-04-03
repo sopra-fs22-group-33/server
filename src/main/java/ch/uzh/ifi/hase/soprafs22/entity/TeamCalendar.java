0a857b2359aec3560a941c6c01a66a3f7cfbe074
“Julia” <juliaprozorova2@gmail.com>
2022-04-03 22:20:54 +0200

    Implement calendar entity with days and timeslots

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/TeamCalendar.java
@@ -0,0 +1,43 @@
+package ch.uzh.ifi.hase.soprafs22.entity;
+import ch.uzh.ifi.hase.soprafs22.constant.Weekday;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Entity
+public class TeamCalendar implements Serializable {
+
+    @Id
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    private Team team;
+
+    @ElementCollection
+    private Map<Weekday, Day> concreteDays = new LinkedHashMap<Weekday, Day>();
+
+}
+
+
+@Embeddable
+class Day {
+
+
+    @ElementCollection
+    private ArrayList<Timeslot> timeslots = new ArrayList<Timeslot>();
+
+}
+
+@Embeddable
+class Timeslot {
+
+    @ManyToMany
+    @JoinColumn(name = "User_id")
+    private ArrayList<User> assignedUsers;
+
+
+}
+
+
