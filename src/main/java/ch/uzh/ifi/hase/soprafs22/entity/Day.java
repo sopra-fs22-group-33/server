7af499e7f972a3971d21495f3e3a86401a5ad74a
vmjulia <juliaprozorova2@gmail.com>
2022-05-15 14:57:16 +0200

    comments

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Day.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Day.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Day implements Serializable {
 
-    // define composite key
+
     @Id
     @GeneratedValue
     private Long id;
@@ -30,7 +30,6 @@ public class Day implements Serializable {
         return teamCalendar;
     }
 
-
     public void setTeamCalendar(TeamCalendar teamcalendar) {
         this.teamCalendar = teamcalendar;
     }
@@ -47,9 +46,7 @@ public class Day implements Serializable {
         return slots;
     }
 
-    public void setSlots(List<Slot> slots){
-        this.slots = slots;}
-
+    public void setSlots(List<Slot> slots){ this.slots = slots;}
 
     public Long getId() {
         return id;
