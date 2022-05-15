03b716734f7486cf3c783fb20226bdd0ccad6c76
vmjulia <juliaprozorova2@gmail.com>
2022-05-15 17:22:46 +0200

    optimizer test

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Optimizer.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Optimizer.java
@@ -320,7 +320,6 @@ public class Optimizer {
                             res = true;
                         }
                     }
-
                 }
             }
         }
--- src/test/java/ch/uzh/ifi/hase/soprafs22/OptimizerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/OptimizerTest.java
@@ -1,10 +1,9 @@
 package ch.uzh.ifi.hase.soprafs22;
 
-import ch.uzh.ifi.hase.soprafs22.entity.Day;
-import ch.uzh.ifi.hase.soprafs22.entity.Schedule;
-import ch.uzh.ifi.hase.soprafs22.entity.Slot;
-import ch.uzh.ifi.hase.soprafs22.entity.TeamCalendar;
+import ch.uzh.ifi.hase.soprafs22.entity.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,23 +17,32 @@ public class OptimizerTest {
     @Test
     public void OptimizerTest_One_User() throws Exception {
         TeamCalendar teamCalendar = new TeamCalendar();
+        teamCalendar.setId(1L);
         Day day = new Day();
+        day.setTeamCalendar(teamCalendar);
         Slot slot = new Slot();
+        slot.setDay(day);
+        slot.setTimeFrom(11);
+        slot.setTimeTo(14);
         Schedule schedule = new Schedule();
         schedule.setSpecial(-1);
         schedule.setBase(1);
+        User user = new User();
+        user.setId(1L);
+        schedule.setUser(user);
+        schedule.setSlot(slot);
         List<Schedule> schedules = Collections.singletonList(schedule);
         slot.setSchedules(schedules);
+        user.setSchedules(schedules);
         slot.setRequirement(1);
         List<Slot> slots = Collections.singletonList(slot);
         day.setSlots(slots);
         List<Day> days = Collections.singletonList(day);
         teamCalendar.setBasePlan(days);
-        //teamCalendar.setStartingDate("123");
+        teamCalendar.setStartingDate(LocalDate.now());
 
-        //Optimizer optimizer = new Optimizer (teamCalendar);
-        //assertTrue(optimizer.isFeasible());
-        //assertEquals(1, schedule.getAssigned());
+        Optimizer optimizer = new Optimizer (teamCalendar);
+        assertEquals(1, schedule.getAssigned());
     }
 
 
