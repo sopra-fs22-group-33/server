880317ebc11905f87b53747c46cedb3ebd1e939d
grueezi <mark.rueetschi@uzh.ch>
2022-05-18 15:40:07 +0200

    user specific tests

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/EmailServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/EmailServiceTest.java
@@ -1,25 +1,15 @@
 package ch.uzh.ifi.hase.soprafs22.service;
 
-import ch.uzh.ifi.hase.soprafs22.entity.Invitation;
-import ch.uzh.ifi.hase.soprafs22.entity.Team;
-import ch.uzh.ifi.hase.soprafs22.entity.User;
-import ch.uzh.ifi.hase.soprafs22.repository.InvitationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-
 public class EmailServiceTest {
 
   @Mock
-  private InvitationRepository invitationRepository;
-
-  EmailService emailService = mock(EmailService.class);
+  private EmailService emailService;
 
   @BeforeEach
   public void setup() {
