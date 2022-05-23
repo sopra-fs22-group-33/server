bc6ee1d2e43275cf0bb8302858f1c8ecbebdba96
vmjulia <juliaprozorova2@gmail.com>
2022-05-23 22:17:39 +0200

    fix bug

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/PlayerPutDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/PlayerPutDTO.java
@@ -1,17 +1,29 @@
 package ch.uzh.ifi.hase.soprafs22.rest.dto;
 
+import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs22.entity.Location;
 
+import javax.persistence.Column;
 import java.util.List;
 
 public class PlayerPutDTO {
     private List<Location> chunks;
 
+
+    private UserStatus statusOnline;
     public List<Location> getChunks() {
         return chunks;
     }
 
     public void setChunks(List<Location> chunks) {
         this.chunks = chunks;
+    }
+
+    public UserStatus getStatusOnline() {
+        return statusOnline;
+    }
+
+    public void setStatusOnline(UserStatus statusOnline) {
+        this.statusOnline = statusOnline;
     }
 }
